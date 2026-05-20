02dcf7b72c09b1152077064bb7451599202e931f
mtfelipe777 <matheuslabanca0@gmail.com>
2026-05-20 16:42:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("batata")
+from conta import Conta
+acesso = Conta()
+
+
+
+while True:
+    print("1.Saca;")
+    print("2.Deposita;")
+    print("3.Calcula Rendimento.")
+    acao = int(input("Informe a opção: "))
+
+    if acao == 1:
+        acesso.saca()
+    elif acao == 2:
+        acesso.deposita()
+    elif acao == 3:
+        acesso.calcula_rendimento()
+    else:
+        break
