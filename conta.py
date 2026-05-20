e6c8fcec7628dd53494edd0248d3cd5a662176f7
Sarah Cristina <sc8479164@gmail.com>
2026-05-20 16:59:05 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -6,7 +6,7 @@ class Conta:
         self.saldo - self.retirada
     def deposita(self):
         self.entrada = int(input("Quanto você quer adicionar?"))
-        self.saldo + self.entrada
+        self.saldo =+ self.entrada
     def calcula_Rendimento(self):
         self.saldo * 0.1
     def imprimir_dados(self):
